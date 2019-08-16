509b910ce481ee3f31d725131b70a78380229b21
stephan <stephansmit@hotmail.com>
2019-08-16 11:45:55 +0200

    variable image name

--- run_su2.py
+++ run_su2.py
@@ -21,26 +21,29 @@ def download_testcase(testcase_dir):
         with open(os.path.join(testcase_dir,url.split('/')[-1]), 'wb') as f:
                 f.write(r.content)
     
-def download_images(image_dir):
+def download_images(image_dir, image_url):
+    image_name='su2_containers_fork_dev.sif'
     try:
         os.mkdir(image_dir)
     except:
         pass
     
-    client.pull(image='shub://stephansmit/su2_containers:fork_dev',name='su2_containers_fork_dev.sif', pull_folder=image_dir)
+    client.pull(image=image_url,name=image_name, pull_folder=image_dir)
+    return image_name
 
-def run_case(image_dir, testcase_dir):
-    client.load(os.path.join(image_dir,'su2_containers_fork_dev.sif'))
-    output = client.execute(['mpirun', '-np', '6', '/SU2/bin/SU2_CFD','/data/Jones.cfg'], bind=testcase_dir+":/data:", options=['--pwd','/data'], stream=True)
+def run_case(image_dir, image_name, testcase_dir):
+    client.load(os.path.join(image_dir,image_name))
+    output = client.execute(['mpirun', '-np', '6', '/SU2/bin/SU2_CFD','Jones.cfg'], bind=testcase_dir+":/data:", options=['--pwd','/data'], stream=True)
     for val in output:
         print(val.strip('\n'))
 
 if __name__=="__main__":
     testcase_dir = 'testcase/'
     image_dir='images/'
+    image_url='shub://stephansmit/su2_containers:fork_dev'
     print("Downloading the testcase")
     download_testcase(testcase_dir)
     print("Downloading the Singularity Images")
-    download_images(image_dir)
+    image_name = download_images(image_dir,image_url)
     print("Running the case")
-    run_case(image_dir,testcase_dir)
+    run_case(image_dir,image_name,testcase_dir)
